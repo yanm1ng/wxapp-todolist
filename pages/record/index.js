7d551c8ea9c8bf7f29ba505762032f1aaf370fff
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-01-28 22:42:50 +0800

    添加了TODO的每日记录，之后增加排序和图表功能

--- pages/record/index.js
+++ pages/record/index.js
@@ -1,5 +1,25 @@
 //index.js
 var app = getApp();
 Page({
-    
+	data: {
+    history: []
+  },
+	onLoad: function () {
+    var history = wx.getStorageSync('todos_history');
+    var rage = [];
+    for (var i = 0; i < history.length; i++) {
+      var todos = history[i].todos;
+      var completed = 0;
+      for (var j = 0; j < todos.length; j++) {
+        if (todos[j].completed) {
+          completed++;
+        }
+      }
+      history[i].rage = completed + '/' + todos.length;
+    }
+
+    this.setData({
+      history
+    })
+  },
 })
